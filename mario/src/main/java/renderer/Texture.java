98d70bbdb92cb7bd9c0a6df6e37e02f9ce8d5f5e
seneshMH <senesh1senesh@gmail.com>
2022-08-24 11:59:43 +0530

    Fixing bug on textute

--- mario/src/main/java/renderer/Texture.java
+++ mario/src/main/java/renderer/Texture.java
@@ -76,4 +76,18 @@ public class Texture {
     public int getTexID() {
         return texID;
     }
+
+    public String getFilePath() {
+        return this.filePath;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof Texture)) return false;
+        Texture oTex = (Texture)o;
+        return oTex.getWidth() == this.width && oTex.getHeight() == this.height &&
+                oTex.getTexID() == this.texID &&
+                oTex.getFilePath().equals(this.filePath);
+    }
 }
